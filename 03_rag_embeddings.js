bc416002e1972e98108f79b94420531fc800cff8
Manik kori <manikkori697@gmail.com>
2026-07-13 14:58:51 +0530

    add RAG Embeddings logic

--- 03_rag_embeddings.js
+++ 03_rag_embeddings.js
@@ -1,28 +1,30 @@
 import { HuggingFaceTransformersEmbeddings } from "@langchain/community/embeddings/huggingface_transformers";
+
 async function textToNum(){
 
     try {
-        //1. Embeddings model initialization
-        const embeddings = new HuggingFaceTransformersEmbeddings({
+        //1. Embeddings model initializing
+        const embeddings =  new HuggingFaceTransformersEmbeddings({
+
             modelName:"Xenova/all-MiniLM-L6-v2",
 
         });
 
-        let myText = "Machine Learning is the future of computer science.";
+        const myText = "Machine Learning is the future of computer science."
         console.log(`My text : ${myText}`);
 
-        //2. text to embed (converting)
-        const vectorMath = await embeddings.embedQuery(myText);
+        //2. text to embed
+        const vector = await embeddings.embedQuery(myText);
+        console.log(`Total number generated : ${vector.length}`);
+        
+        console.log("first 5 number printing....\n");
 
-        console.log(`total numbers generated : ${vectorMath.length}`);
+        console.log(vector.slice(0, 5));
+        
+        
 
-        console.log("first 5 number printing...\n");
-        console.log(vectorMath.slice(0,5));
-        
-        
-        
     } catch (error) {
-        console.log("error", error.message);
+        console.log("error",error.message);
         
     }
 
