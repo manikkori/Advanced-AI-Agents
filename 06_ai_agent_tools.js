abf90aba0281c5c27459c361e8a282395152786d
Manik <manikkori697@gmail.com>
2026-08-09 18:37:11 +0530

    Added new tool (Live crypto price)

--- 06_ai_agent_tools.js
+++ 06_ai_agent_tools.js
@@ -1,7 +1,7 @@
 import { ChatGroq } from "@langchain/groq";
 import { tool } from "@langchain/core/tools";
 import { createReactAgent } from "langchain/agents";
-import { z } from "zod";
+import { symbol, z } from "zod";
 import "dotenv/config";
 import { Schema } from "zod/v3";
 
@@ -26,6 +26,31 @@ const weatherTool = tool(
     }
 )
 
+//tool 2 - Live crypto price
+const cryptoTool = tool(
+    async ({symbol}) => {
+        try {
+            //binance free api
+            const response = await fetch(`https://api.binance.com/api/v3/ticker/price?symbol=${symbol.toUpperCase()}USDT`)
+            const data = await response.json();
+            return `Current price of ${symbol} is ${data.price}`;
+
+
+        } catch (error) {
+            return `could not fetch error :${error}`;
+        }
+    },
+    {
+        name:"get_crypto_price",
+        description:"Fetches the real-time live price of a cryptocurrency in USD. Input MUST be the crypto symbol (e.g., BTC, ETH, DOGE).",
+        schema: z.object({
+            symbol: z.string().describe("The exact cryptocurrency symbol (like BTC, ETH)"),
+        }),
+
+    }
+);
+
+
 async function main(){
 
     console.log("\nConnecting AI Agent, please wait....\n");
@@ -37,7 +62,8 @@ async function main(){
         temperature:0,
     });
 
-    const tools = [weatherTool]
+    //2. giving tools
+    const tools = [weatherTool, cryptoTool];
 
 
     
