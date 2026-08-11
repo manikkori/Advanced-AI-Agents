50cff34ef9f4d62bb1d035a6a90453c6432e8636
Manik <manikkori697@gmail.com>
2026-08-11 12:04:36 +0530

    Setup Agent memory and llm

--- 08_agent_memory.js
+++ 08_agent_memory.js
@@ -3,6 +3,7 @@ import { createReactAgent } from "@langchain/langgraph/prebuilt";
 import { MemorySaver } from "@langchain/langgraph";
 import * as readline from "readline"; // nodejs inbuilt method for terminal input
 import "dotenv/config";
+import { resolve } from "dns";
 
 
 //terminal input setup
@@ -11,9 +12,24 @@ const rl = readline.createInterface({
     output:process.stdout
 });
 
+//function - waiting to type in the terminal.
+const askQestion = (query) => new Promise((resolve)=> rl.question(query, resolve));
+
 async function main(){
 
-    console.log("everything is ok!");
+    console.log("\nAgent Memory starting...\n");
+
+    //1. Memory setup
+    const memory = new MemorySaver();
+
+    //2. LLM setup 
+    const llm = new ChatGroq({
+        apiKey:process.env.GROQ_API_KEY,
+        model:"openai/gpt-oss-20b",
+        temperature:0
+    });
+    
+    
     
 
 }
