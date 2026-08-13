7a4d2833f26a132ad390b50ec42ecd25938d4c13
Manik <manikkori697@gmail.com>
2026-08-13 14:09:38 +0530

    Setup database connection

--- 10_sql_agent.js
+++ 10_sql_agent.js
@@ -9,8 +9,36 @@ const llm = new ChatGroq({
     temperature:0,
 });
 
+/* 
+========================================================
+📂 DATABASE SCHEMA INFO (For Reference)
+========================================================
+Table Name: ai_agent_students
+Columns:
+- id (INT) - Primary Key
+- name (VARCHAR)
+- marks (INT)
+- course (VARCHAR)
+(Note: Table and data already exist in the Aiven Database)
+========================================================
+*/
+
 async function main(){
-    console.log("ok!");
+    console.log("\n Connecting to MySql Database!\n");
+
+    const datasource = new DataSource({
+        type:"mysql",
+        url:process.env.MYSQL_URI,
+        synchronize:false,
+        ssl: {rejectUnauthorized:false}
+    });
+
+    await datasource.initialize();
+    console.log("Connected to database !\n");
+    
+
+    
+    
 }
 
 main().catch(console.error);
