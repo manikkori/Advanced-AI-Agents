7fd7287e1b50cf677a0ed6c3bb00c19976c7d72f
Manik <manikkori697@gmail.com>
2026-08-13 14:26:26 +0530

    Added sql query generate logic

--- 10_sql_agent.js
+++ 10_sql_agent.js
@@ -4,9 +4,9 @@ import "dotenv/config";
 
 // llm connection
 const llm = new ChatGroq({
-    apiKey:process.env.GROQ_API_KEY,
-    model:"openai/gpt-oss-120b",
-    temperature:0,
+    apiKey: process.env.GROQ_API_KEY,
+    model: "openai/gpt-oss-120b",
+    temperature: 0,
 });
 
 /* 
@@ -23,22 +23,41 @@ Columns:
 ========================================================
 */
 
-async function main(){
+async function main() {
     console.log("\n Connecting to MySql Database!\n");
+    //1. connecting to database
 
     const datasource = new DataSource({
-        type:"mysql",
-        url:process.env.MYSQL_URI,
-        synchronize:false,
-        ssl: {rejectUnauthorized:false}
+        type: "mysql",
+        url: process.env.MYSQL_URI,
+        synchronize: false,
+        ssl: { rejectUnauthorized: false }
     });
 
     await datasource.initialize();
     console.log("Connected to database !\n");
-    
 
-    
-    
+    const question = "tell me the name of the student whose name starts with 'M'."
+    console.log(`[User question]: ${question}\n`);
+
+    //2. llm generates sql query
+    const prompt1 = `You are a MySQL query generator. Based on the user's question, output ONLY a valid MySQL query. 
+
+        Database Details:
+        - Table Name: ai_agent_students
+        - Columns: id (INT), name (VARCHAR), marks (INT), course (VARCHAR)
+
+        Rules:
+        1. Output ONLY the raw SQL query. No explanation, no markdown backticks and no extra words.
+        2. Only generate SELECT queries to read data.
+
+        User Question: ${question}
+        SQL Query:
+    `;
+
+    const sqlQuery = await llm.invoke(prompt1);
+    console.log(`[Generated query]: ${sqlQuery.content}`);
+
 }
 
 main().catch(console.error);
