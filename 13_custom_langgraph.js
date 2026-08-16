5656cb9bc8ec8d9799089e941ea6c2d725319ee9
Manik <manikkori697@gmail.com>
2026-08-16 17:17:24 +0530

    Added note named writerNode

--- 13_custom_langgraph.js
+++ 13_custom_langgraph.js
@@ -4,20 +4,33 @@ import "dotenv/config";
 
 //1. setup llm
 const llm = new ChatGroq({
-    apiKey:process.env.GROQ_API_KEY,
-    model:"openai/gpt-oss-120b",
-    temperature:0.7,
+    apiKey: process.env.GROQ_API_KEY,
+    model: "openai/gpt-oss-120b",
+    temperature: 0.7,
 });
 
 //2. define state , graph's globle memory
 const stateGraph = Annotation.Root({
-    topic:Annotation(),
-    draft:Annotation(),
-    review:Annotation()
+    topic: Annotation(),
+    draft: Annotation(),
+    review: Annotation()
 });
 
-async function main(){
+//3. define node, worker
+//A. writer
+async function writerNode(state) {
+    console.log(`[Writer Node]: Drafting content for -> "${state.topic}."`);
+    const prompt = `Write a short, engaging fact about: ${state.topic}.`;
+    const response = await llm.invoke(prompt);
+
+    //update draft
+    return { draft: response.content }
+
+}
+
+
+async function main() {
     console.log("everything is ok!");
-    
+
 }
 main().catch(console.error);
