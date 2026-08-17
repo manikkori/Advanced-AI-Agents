f8a52b564cd689d7ff562768c59695fdbb0a46bf
Manik <manikkori697@gmail.com>
2026-08-17 19:32:18 +0530

    Install nodemailer

---
+++ 14_human_approval.js
@@ -0,0 +1,14 @@
+import { ChatGroq } from "@langchain/groq";
+import { MemorySaver } from "@langchain/langgraph";
+import { StateGraph, START, END, Annotation } from "@langchain/langgraph";
+import * as readline from "readline";
+import "dotenv/config";
+
+
+async function main(){
+
+    console.log("everything is ok!");
+    
+
+}
+main().catch(console.error)
