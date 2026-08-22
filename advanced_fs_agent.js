03c6853dc48f90e4dce678a932ff9f866ba159c3
Manik <manikkori697@gmail.com>
2026-08-22 13:16:15 +0530

    Setup readline for user terminal input

--- advanced_fs_agent.js
+++ advanced_fs_agent.js
@@ -1,11 +1,19 @@
 import { ChatGroq } from "@langchain/groq";
 import "dotenv/config";
+import * as rl from "readline/promises";
+
+const readline = rl.createInterface({
+    input:process.stdin,
+    output:process.stdout,
+});
+
 //setup llm
 const llm  = new ChatGroq({
     apiKey:process.env.GROQ_API_KEY,
     model:"openai/gpt-oss-120b",
     temperature:0.2
 });
+
 
 async function main(){
 
