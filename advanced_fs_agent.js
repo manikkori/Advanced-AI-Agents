a3f52d4f0e9c46b7a1e7fd3ecdddee6f4377c2b5
Manik <manikkori697@gmail.com>
2026-08-22 13:28:47 +0530

    install zod for strict schema and create a tool (create, write file)

--- advanced_fs_agent.js
+++ advanced_fs_agent.js
@@ -1,6 +1,9 @@
 import { ChatGroq } from "@langchain/groq";
+import { tool } from "@langchain/core/tools";
 import "dotenv/config";
 import * as rl from "readline/promises";
+import * as fs from "fs/promises"
+import * as z from "zod";
 
 const readline = rl.createInterface({
     input:process.stdin,
@@ -14,11 +17,34 @@ const llm  = new ChatGroq({
     temperature:0.2
 });
 
+//2. define tool 
+//a. create and write file
+const createFileTool = tool(
+    async ({file_name, content}) =>{
+        console.log(`[AI]: Creating file: ${file_name}...\n`);
+        try {
+            await fs.writeFile(file_name, content, "utf-8");
+            return `Success: file ${file_name} created successfully.`;
+        } catch (error) {
+            return `[Error]: ${error.message}`;
+            
+        }
+        
+    },
+    {
+        name:"create_file",
+        description:"Creates a  new file and overwrites an existing file with code or text.",
+        schema: z.object({
+            file_name: z.string().describe("name of the file like -> index.html, script.js, my.txt etc."),
+            content: z.string().describe("the full code or text to write inside the file."),
+        })
+    }
+);
 
 async function main(){
 
     console.log("Everything is ok!");
-    
+    readline.close();
 }
 
 main().catch(console.error)
